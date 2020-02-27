61677d65b4e984d76be463a8966a56c848e49c69
Vici Shaweddy <vshaweddy@gmail.com>
2020-02-27 14:28:50 -0500

    added models

---
+++ Worn Out/Worn Out/Models/Run.swift
@@ -0,0 +1,16 @@
+//
+//  Run.swift
+//  Worn Out
+//
+//  Created by Vici Shaweddy on 2/27/20.
+//  Copyright © 2020 Vici Shaweddy. All rights reserved.
+//
+
+import Foundation
+
+struct Run {
+    let workoutIdentifier: String //HKObject UUID
+    let startDate: Date
+    let miles: Double
+    let shoe: Shoe
+}
