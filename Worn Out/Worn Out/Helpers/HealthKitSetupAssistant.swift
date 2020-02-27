ac27d85ebc207e6c41f33e6382ff562dec0c1d36
Vici Shaweddy <vshaweddy@gmail.com>
2020-02-26 21:37:18 -0500

    adding HealthKitSetupAssistant

---
+++ Worn Out/Worn Out/Helpers/HealthKitSetupAssistant.swift
@@ -0,0 +1,34 @@
+//
+//  HealthKitSetupAssistant.swift
+//  Worn Out
+//
+//  Created by Vici Shaweddy on 2/26/20.
+//  Copyright © 2020 Vici Shaweddy. All rights reserved.
+//
+
+import HealthKit
+
+class HealthKitSetupAssistant {
+    private enum HealthKitSetupError: Error {
+        case notAvailableOnDevice
+        case dataTypeNotAvailable
+    }
+    
+    class  func authorizeHealtKit(completion: @escaping (Bool, Error?) -> Swift.Void) {
+        // Check to see if HealthKit Is Available on this device
+        guard HKHealthStore.isHealthDataAvailable() else {
+            completion(false, HealthKitSetupError.notAvailableOnDevice)
+            return
+        }
+        
+        // Prepare a list of types you want HealthKit to read and write
+        let healthKitTypesToWrite: Set<HKSampleType> = [HKObjectType.workoutType()]
+        
+        let healtKitTypesToRead: Set<HKObjectType> = [HKObjectType.workoutType()]
+        
+        // Request Authorization
+        HKHealthStore().requestAuthorization(toShare: healthKitTypesToWrite, read: healtKitTypesToRead) { (success, error) in
+            completion(success, error)
+        }
+    }
+}
