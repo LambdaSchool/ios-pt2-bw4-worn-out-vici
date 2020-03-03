43745ce4d9bd7894d834c7014e47851b3caac6f9
Vici Shaweddy <vshaweddy@gmail.com>
2020-03-03 17:32:16 -0500

    populating the summary cell

--- Worn Out/Worn Out/Views/SummaryTableViewCell.swift
+++ Worn Out/Worn Out/Views/SummaryTableViewCell.swift
@@ -9,16 +9,27 @@
 import UIKit
 
 class SummaryTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var totalMilesLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var brandLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    }
+    
+    func configureWithRun(run: Run) {
+        self.totalMilesLabel.text = String(run.miles)
+//        self.timeLabel.text = run.startDate
+    }
+    
+    func configureWithShoe(shoe: Shoe) {
+        self.nicknameLabel.text = shoe.nickname
+        self.brandLabel.text = shoe.brand
     }
 
 }
