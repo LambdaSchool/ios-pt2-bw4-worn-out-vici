9047dca8fdb1ba80bf87281001c649b7b24b6766
Vici Shaweddy <vshaweddy@gmail.com>
2020-03-05 20:48:26 -0500

    adding progress view for shoe age

---
+++ Worn Out/Worn Out/Views/ShoeSelectionHeaderView.swift
@@ -0,0 +1,50 @@
+//
+//  ShoeSelectionHeaderView.swift
+//  Worn Out
+//
+//  Created by Vici Shaweddy on 3/5/20.
+//  Copyright © 2020 Vici Shaweddy. All rights reserved.
+//
+
+import UIKit
+
+class ShoeSelectionHeaderView: UITableViewHeaderFooterView {
+    private lazy var stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layoutMargins = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        view.isLayoutMarginsRelativeArrangement = true
+        view.spacing = 8
+        return view
+    }()
+    
+    private lazy var headerLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        self.setupStackView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupStackView() {
+        self.contentView.addSubview(self.stackView)
+        NSLayoutConstraint.activate([
+            self.stackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            self.stackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            self.stackView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            self.stackView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+        ])
+        self.stackView.addArrangedSubview(headerLabel)
+        self.headerLabel.text = "Selected Shoes"
+    }
+    
+}
---
+++ Worn Out/Worn Out/Views/ShoeSelectionTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoeSelectionTableViewCell.swift
+//  Worn Out
+//
+//  Created by Vici Shaweddy on 3/5/20.
+//  Copyright © 2020 Vici Shaweddy. All rights reserved.
+//
+
+import UIKit
+
+class ShoeSelectionTableViewCell: UITableViewCell {
+    @IBOutlet weak var shoeTextField: UITextField!
+}
